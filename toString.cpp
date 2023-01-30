ee1d6baeaf9399259cbdf05044f10808ca0edea9
Monica 1900199 <104925298+MonicaMickie@users.noreply.github.com>
2023-01-30 09:08:09 +0200

    Create toString.cpp

---
+++ toString.cpp
@@ -0,0 +1,16 @@
+vector<string> split(string str, string token){
+    vector<string>result;
+    while(str.size()){
+        int index = str.find(token);
+        if(index!=string::npos){
+            result.push_back(str.substr(0,index));
+            str = str.substr(index+token.size());
+            if(str.size()==0)result.push_back(str);
+        }
+        else{
+            result.push_back(str);
+            str = "";
+        }
+    }
+    return result;
+}
