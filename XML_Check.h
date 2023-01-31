740ad4f33a6a74dea0f45942f5caecf8fb4456ad
Monica 1900199 <104925298+MonicaMickie@users.noreply.github.com>
2023-01-31 23:12:46 +0200

    Create XML_Check.h

---
+++ XML_Check.h
@@ -0,0 +1,13 @@
+#ifndef _XML_Check_
+#define _XML_Check_
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <regex>
+#include <stack>
+using namespace std;
+
+void XML_Check(string xml);
+
+#endif
