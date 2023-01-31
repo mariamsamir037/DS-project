b9a88ceafa0195f496b73546c7ee37024215e715
Monica 1900199 <104925298+MonicaMickie@users.noreply.github.com>
2023-01-31 23:37:20 +0200

    Create labels.h

---
+++ Header files/labels.h
@@ -0,0 +1,20 @@
+#ifndef labels
+#define labels
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class labels{
+
+public:
+	int bar;
+	int margin;
+	string features;
+
+public:
+	labels(string f, int b, int m);
+};
+
+#endif
