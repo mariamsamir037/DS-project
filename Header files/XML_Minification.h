b422ddb735db146f71fea8a3e763cb44c73ca914
Monica 1900199 <104925298+MonicaMickie@users.noreply.github.com>
2023-01-31 23:26:10 +0200

    Create XML_Minification.h

---
+++ Header files/XML_Minification.h
@@ -0,0 +1,13 @@
+
+#ifndef _XML_Minification_
+#define _XML_Minification_
+
+#include <iostream>
+#include <string>
+#include<algorithm>
+
+using namespace std;
+
+void minify_XML(string xml);
+
+#endif
