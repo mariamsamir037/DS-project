7f8b1ee67a7e01b926f3e0bfa0c6d94b84ef52d9
Monica 1900199 <104925298+MonicaMickie@users.noreply.github.com>
2023-01-31 23:19:40 +0200

    Create XML_Correction.h

---
+++ XML_Correction.h
@@ -0,0 +1,14 @@
+#ifndef _XML_Correction_
+#define _XML_Correction_
+
+#include <iostream>
+#include <stack>
+#include <string>
+#include<algorithm>
+#include "labels.cpp"
+
+using namespace std;
+
+void correct_XML(vector<string> XML);
+
+#endif
